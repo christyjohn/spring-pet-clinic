947576f46d518cf76965b2dbe104f5e4ce9b53c0
Christy John <christyjohn.crz@gmail.com>
2022-05-17 11:38:11 +0530

    Refactor Service Interfaces to Common Base Interface. Closes #10.

--- pet-clinic-data/src/main/java/me/christyjohn/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/me/christyjohn/springpetclinic/services/VetService.java
@@ -1,7 +1,5 @@
 package me.christyjohn.springpetclinic.services;
 
-import java.util.Set;
-
 import me.christyjohn.springpetclinic.model.Vet;
 
 public interface VetService extends CrudService<Vet, Long> {
