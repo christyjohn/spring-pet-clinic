a255dfd0e9b96bbb2d83094043f4eb3ec87a08fd
Christy John <christyjohn.crz@gmail.com>
2022-05-18 00:10:47 +0530

    List all vets. Closes #14

--- pet-clinic-web/src/main/java/me/christyjohn/springpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/me/christyjohn/springpetclinic/controller/VetController.java
@@ -1,15 +1,24 @@
 package me.christyjohn.springpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import me.christyjohn.springpetclinic.service.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
-	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listVets() {
-		
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
