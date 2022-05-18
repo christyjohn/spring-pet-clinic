4385d816cdb56317345ee05ff4a215a08e29f63f
Christy John <christyjohn.crz@gmail.com>
2022-05-18 22:35:31 +0530

    Added specialities Map impl. Closes #46

---
+++ pet-clinic-data/src/main/java/me/christyjohn/springpetclinic/service/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package me.christyjohn.springpetclinic.service;
+
+import me.christyjohn.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
